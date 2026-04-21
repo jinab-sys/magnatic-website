58b9a0e35117bd9206aac1b31a98c95128db4f77
Hassaan Bin Sajid <hassaan@auctionnow.com>
2026-04-21 15:37:19 +0500

    added heatmap

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import { Bricolage_Grotesque, Instrument_Sans, IBM_Plex_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -44,6 +45,15 @@ export default function RootLayout({
         className={`${instrumentSans.variable} ${bricolageGrotesque.variable} ${ibmPlexMono.variable} font-sans antialiased text-[color:var(--mag-fg)]`}
       >
         {children}
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "wf49mi79bq");
+          `}
+        </Script>
       </body>
     </html>
   );
